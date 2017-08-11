b4618d618409e5148af910e74cbb6908b9330122
YeamGood <yeamgood@gmail.com>
2017-08-12 02:47:21 +0700

    add i18n

---
+++ src/main/java/com/yeamgood/godungonline/configuration/I18nConfiguration.java
@@ -0,0 +1,21 @@
+package com.yeamgood.godungonline.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class I18nConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
---
+++ src/main/resources/templates/fragments/footer.html
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <div th:fragment="footer">
+      &copy; 2016 Footer
+    </div>
+  </body>
+</html>
